e789f4319e8eff6ec6e825fbb432b9501663f9db
Yukai Tu <ytu@lino.network>
2018-09-14 14:48:37 -0700

    add lino username check

---
+++ utils/utils.go
@@ -0,0 +1,24 @@
+package util
+
+// nolint
+import (
+	"regexp"
+)
+
+const (
+	usernameReCheck        = "^[a-z]([a-z0-9-\\.]){1,19}[a-z0-9]$"
+	illegalUsernameReCheck = "^[a-z0-9\\.-]*([-\\.]){2,}[a-z0-9\\.-]*$"
+)
+
+func CheckUsername(username string) bool {
+	match, err := regexp.MatchString(usernameReCheck, username)
+	if err != nil || !match {
+		return false
+	}
+
+	match, err = regexp.MatchString(illegalUsernameReCheck, username)
+	if err != nil || match {
+		return false
+	}
+	return true
+}
